de20c8903b46f1559a77826a13419b550933a8ef
Teddy Blomgren <Teddy_95@live.se>
2024-10-28 10:22:29 +0100

    Added Loopmethod

---
+++ GitDemo2/Helpers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitDemo2
+{
+    internal class Helpers
+    {
+        public static void MyLoop()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
